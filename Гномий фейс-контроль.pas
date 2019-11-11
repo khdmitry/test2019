cf11191c491179fd67abd211de4bf4573ca76462
khdmitry <57549534+khdmitry@users.noreply.github.com>
2019-11-11 20:45:15 +0200

    Add files via upload

---
+++ Гномий фейс-контроль.pas
@@ -0,0 +1,20 @@
+﻿
+  var
+  L, B, W, G, H, E, A: integer;
+  
+
+  begin
+    
+   L:=132;
+   B:=40;
+   W:=4;
+   G:=8;
+   H:=1;
+   E:=8;
+   A:=222;
+   
+   if (((100<=L)and(L<=120))and(B>30)and(W<>0)and(G>5)and((4<=H)and(H<=7))and(E<>0)and(A>L)) then begin
+     write('True');
+     end else write('False');
+   
+end.
---
+++ Счастливые билетики.pas
@@ -0,0 +1,68 @@
+﻿const
+  y = 10; 
+
+  var
+  a: array[1..y] of integer;
+  i, x , m , n , z , b ,c,l: integer;
+  
+
+  begin
+    readln(x);
+    if x mod 2 <> 0 then begin
+      write('ERROR');
+      exit;
+    end;
+    
+    b:=0;
+    c:=1;
+    
+    for i := 1 to x do  
+      begin
+        a[i]:=0;  
+      end;
+    
+    for i := 1 to x do  
+      begin
+        c:=c*10;  
+      end;
+ 
+       
+      
+      
+    for l := 1 to c do begin 
+   
+    m:=0;
+    n:=0;
+    z:=x div 2;
+    
+     for i := 0 to z-1 do  
+       begin
+       m+=a[x-i];
+       n+=a[z-i];
+       end;
+       
+   if m=n then b+=1;
+ 
+  
+  
+  for i := x  downto 1 do 
+    begin
+      if (a[i] < 9) then begin
+        
+        a[i]+=1;
+        
+        break;
+      end else  begin
+      a[i]:=0;
+      end ;
+  end;
+  
+  
+ 
+   
+
+   
+   end;
+       writeln(b);
+   
+end.
